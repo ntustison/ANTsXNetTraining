9caaffcf1f9a51453641058846558680c011dc4a
Nick Tustison <ntustison@gmail.com>
2023-07-24 16:26:19 -0700

    ENH:  Change to loss

--- MRIModalityClassification/train_model_pytorch.py
+++ MRIModalityClassification/train_model_pytorch.py
@@ -242,22 +242,22 @@ def test_loop(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    return correct
+    return correct, loss
 
 
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
 
-current_accuracy = 0.0
+current_loss = 0.0
 
 epochs = 200
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
-    accuracy = test_loop(test_dataloader, model, loss_fn)
-    if accuracy > current_accuracy:
-        print("Accuracy improved.")
+    accuracy, loss = test_loop(test_dataloader, model, loss_fn)
+    if loss < current_loss:
+        print("Loss improved.")
         torch.save(model.state_dict(), weights_filename)
-        current_accuracy = accuracy
+        current_loss = loss
 print("Done!")
 
