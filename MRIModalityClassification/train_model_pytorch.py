9f660a0e63c6f1e95a1218af7c3613ca94e731b5
Nick Tustison <ntustison@gmail.com>
2023-07-24 16:34:52 -0700

    ENH:  Change to loss

--- MRIModalityClassification/train_model_pytorch.py
+++ MRIModalityClassification/train_model_pytorch.py
@@ -242,7 +242,7 @@ def test_loop(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-    return correct, loss
+    return correct, test_loss
 
 
 loss_fn = nn.CrossEntropyLoss()
