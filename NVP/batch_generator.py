b64ca470f1680dec9316ec0ba4d65c6de9718054
Nick Tustison <ntustison@gmail.com>
2025-02-18 10:27:12 -0800

    BUG:  Debugging.

---
+++ NVP/batch_generator.py
@@ -0,0 +1,145 @@
+import numpy as np
+import random
+import ants
+import antspynet
+import time
+
+def batch_generator(batch_size,
+                    input_image_files,
+                    image_size,
+                    do_histogram_intensity_warping=True,
+                    do_histogram_equalization=True,
+                    do_histogram_rank_intensity=True,
+                    do_simulate_bias_field=True,
+                    do_add_noise=True,
+                    do_random_transformation=True,
+                    do_random_contralateral_flips=True,
+                    do_resampling=True,
+                    verbose=False
+                    ):
+
+    X = np.zeros((batch_size, *image_size))
+
+    while True:
+
+        batch_count = 0
+
+        while batch_count < batch_size:
+
+            r_idx = random.sample(list(range(len(input_image_files))), 1)[0]
+
+            if verbose:
+                print("Batch count: ", batch_count)
+
+            images = list()
+            for i in range(1):
+                image = ants.image_read(input_image_files[r_idx])
+                image = image.iMath("Normalize")
+                if do_resampling:
+                    resample_spacing = np.random.uniform(low=0.7, high=1.2, size=3) 
+                    downsampled_image = ants.resample_image(image, resample_params=resample_spacing,
+                                                            use_voxels=False, interp_type=0)
+                    image = ants.resample_image_to_target(downsampled_image, image)
+                images.append(image)
+
+            if do_random_transformation:
+                tic = time.perf_counter()
+                data_augmentation = antspynet.randomly_transform_image_data(images[0],
+                    [images],
+                    number_of_simulations=1,
+                    transform_type='deformation',
+                    sd_affine=0.01,
+                    deformation_transform_type="bspline",
+                    number_of_random_points=1000,
+                    sd_noise=10.0,
+                    number_of_fitting_levels=4,
+                    mesh_size=1,
+                    sd_smoothing=4.0,
+                    input_image_interpolator='linear',
+                    segmentation_image_interpolator='nearestNeighbor')
+                images = data_augmentation['simulated_images'][0]
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Do random transformation {toc - tic:0.4f} seconds.")
+
+            if do_random_contralateral_flips and random.sample((True, False), 1)[0]:
+                tic = time.perf_counter()
+                for i in range(len(images)):
+                    image_array = images[i].numpy()
+                    image_array = np.flip(image_array, axis=0)
+                    images[i] = ants.from_numpy_like(image_array, images[i])
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Random contralateral flip {toc - tic:0.4f} seconds")
+
+            if do_histogram_intensity_warping: # and random.sample((True, True, False), 1)[0]:
+                tic = time.perf_counter()
+                for i in range(len(images)):
+                    break_points = [0.2, 0.4, 0.6, 0.8]
+                    displacements = list()
+                    for b in range(len(break_points)):
+                        displacements.append(abs(random.gauss(0, 0.05)))
+                        if random.sample((True, False), 1)[0]:
+                            displacements[b] *= -1
+                    images[i] = antspynet.histogram_warp_image_intensities(images[i],
+                        break_points=break_points, clamp_end_points=(True, False),
+                        displacements=displacements)
+                    images[i] = (images[i] - images[i].min()) / (images[i].max() - images[i].min())
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Histogram warping {toc - tic:0.4f} seconds")
+
+            if do_histogram_equalization and random.sample((False, False, True), 1)[0]:
+                tic = time.perf_counter()
+                image = ants.histogram_equalize_image(image)
+                image = image.iMath("Normalize")
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Histogram equalization {toc - tic:0.4f} seconds")
+
+            if do_histogram_rank_intensity and random.sample((False, False, True), 1)[0]:
+                tic = time.perf_counter()
+                image = ants.rank_intensity(image)
+                image = image.iMath("Normalize")
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Histogram rank intensity {toc - tic:0.4f} seconds")
+
+            if do_simulate_bias_field and random.sample((True, True, False), 1)[0]:
+                tic = time.perf_counter()
+                for i in range(len(images)):
+                    log_field = antspynet.simulate_bias_field(images[i], 
+                                                              number_of_points=100, 
+                                                              sd_bias_field=0.1, 
+                                                              number_of_fitting_levels=2, 
+                                                              mesh_size=10)
+                    log_field = log_field.iMath("Normalize")
+                    field_array = np.power(np.exp(log_field.numpy()), random.sample((2, 3), 1)[0])
+                    images[i] = images[i] * ants.from_numpy_like(field_array, images[i])
+                    images[i] = (images[i] - images[i].min()) / (images[i].max() - images[i].min())
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Simulate bias field {toc - tic:0.4f} seconds")
+
+            if do_add_noise and random.sample((True, True, False), 1)[0]:
+                tic = time.perf_counter()
+                for i in range(len(images)):
+                    image_ones = images[i] * 0 + 1                
+                    noise_parameters = (0.1, random.uniform(0.05, 0.15))
+                    noise = ants.add_noise_to_image(image_ones, noise_model="additivegaussian", noise_parameters=noise_parameters)
+                    noise = ants.smooth_image(noise, sigma=random.uniform(0.25, 0.5))
+                    images[i] = images[i] * noise
+                    images[i] = images[i].iMath("Normalize")
+                toc = time.perf_counter()
+                if verbose:
+                    print(f"    Add noise {toc - tic:0.4f} seconds")
+
+            for i in range(len(images)):                
+                images[i] = ants.iMath_normalize(images[i]) 
+                X[batch_count,:,:,i] = images[i].numpy()
+                                
+            if batch_count >= batch_size:
+                break
+
+        yield X
+
---
+++ NVP/train_model.py
@@ -0,0 +1,84 @@
+import ants
+import antspynet
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["ITK_DEFAULT_GLOBAL_NUMBER_OF_THREADS"] = "4"
+
+import glob
+
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.backend as K
+
+from batch_generator import batch_generator
+from create_normalizing_flow_model import create_normalizing_flow_model
+
+K.clear_session()
+# gpus = tf.config.experimental.list_physical_devices("GPU")
+# if len(gpus) > 0:
+#     tf.config.experimental.set_memory_growth(gpus[0], True)
+
+# tf.compat.v1.disable_eager_execution()
+
+base_directory = '/home/ntustison/Data/NVP/'
+scripts_directory = base_directory + 'Scripts/'
+
+data_directory = '/home/ntustison/Data/Inpainting/AlignedToNKISlices/'
+brain_slice_files = glob.glob(data_directory + "*/Images/axial/*.nii.gz")
+
+print("Total number of training brain slices: ", str(len(brain_slice_files)))
+
+################################################
+#
+#  Create the model and load weights
+#
+################################################
+
+# Copy from https://github.com/aganse/flow_models/blob/main/train.py
+
+image_size = (256, 256, 1)
+
+nvp_model = create_normalizing_flow_model((image_size), 
+    hidden_layers=[512, 512], flow_steps=6, regularization=0.0,
+    validate_args=False)
+nvp_model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=2e-4))
+
+weights_filename = scripts_directory + "nvp_t1_axial.h5"
+
+if os.path.exists(weights_filename):
+    print("Loading " + weights_filename)
+    nvp_model.load_weights(weights_filename)
+
+###
+#
+# Set up the training generator
+#
+
+batch_size = 64
+
+generator = batch_generator(batch_size=batch_size,
+                            input_image_files=brain_slice_files,
+                            image_size=image_size,
+                            do_histogram_intensity_warping=True,
+                            do_simulate_bias_field=False,
+                            do_add_noise=False,
+                            do_random_transformation=False,
+                            do_random_contralateral_flips=False,
+                            do_resampling=False,
+                            verbose=False)
+
+track = nvp_model.fit(x=generator, epochs=10, verbose=1, steps_per_epoch=32,
+    callbacks=[
+       keras.callbacks.ModelCheckpoint(weights_filename, monitor='loss',
+           save_best_only=True, save_weights_only=True, mode='auto', verbose=1),
+       keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.95,
+          verbose=1, patience=20, mode='auto'),
+    #    keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.000001,
+    #       patience=20)
+       ]
+   )
+
+nvp_model.save_weights(weights_filename)
+
+
